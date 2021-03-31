b7be68c42a04d55c31b75ed3e1bbbf8a7f36b734
kaibob222 <43156001+kaibob222@users.noreply.github.com>
2021-03-31 20:31:40 +0900

    add classes

---
+++ Classes/Game.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __GAME_SCENE_H__
+#define __GAME_SCENE_H__
+#include "cocos2d.h"
+
+
+
+
+class Game
+{
+};
+
+#endif
+
---
+++ Classes/Unit.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef __UNIT_SCENE_H__
+#define __UNIT_SCENE_H__
+#include "cocos2d.h"
+
+class Unit
+{
+};
+
+
+#endif
